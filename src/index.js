2827287eaad66cf915b5cfeeecf866c80b2034f6
Your Name <fkmks247@gmail.com>
2018-02-02 19:14:48 +0900

    use HashRouter for gh-pages

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {BrowserRouter, Route, Switch} from 'react-router-dom'
+import {HashRouter, BrowserRouter, Route, Switch} from 'react-router-dom'
 import App from './containers/App'
 import registerServiceWorker from './registerServiceWorker'
 import 'bootstrap/dist/css/bootstrap.css'
 import './index.css'
 
 ReactDOM.render((
-  <BrowserRouter>
+  <HashRouter>
     <Switch>
       <Route path='/' name='Personalize' component={App} />
     </Switch>
-  </BrowserRouter>
+  </HashRouter>
 ), document.getElementById('root'))
 
 registerServiceWorker()
