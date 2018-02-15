fb67cbb8dc0fc21680403a5ba3982c4e8c2620bd
Your Name <fkmks247@gmail.com>
2018-02-15 18:11:14 +0900

    Imeplement Product page with google-spreadsheet

--- src/pages/Products/Products.js
+++ src/pages/Products/Products.js
@@ -1,4 +1,7 @@
 import React, {Component} from 'react'
+import axios from 'axios'
+import _ from 'lodash'
+import Spinner from 'react-spinkit'
 import {
   Container,
   Button,
@@ -12,18 +15,25 @@ import FaLightbulb from 'react-icons/lib/fa/lightbulb-o'
 import FaUserMd from 'react-icons/lib/fa/user-md'
 
 import COLOR from '../../consts/color'
+import * as API from '../../utils/api'
 
-const SECOND_STICKY_HEADER_POS_TOP = 104
+const SECOND_STICKY_HEADER_POS_TOP = 100
 const containerStyle = { maxWidth: 1400 }
 const filterItemColStyle = { minWidth: 115 }
 const filterItemStyle = { minWidth: 115 }
 const productsColStyle = { }
 const productPanelTitleStyle = {
-  left: 0,
   margin: '1.625em 20px',
   position: 'absolute',
-  right: 0,
   top: 0,
+  left: 14,
+  right: 0,
+}
+
+const initialState = {
+  categories: [],
+  products: [],
+  filters: [],
 }
 
 /**
@@ -46,9 +56,9 @@ function FilterItem (props) {
   )
 }
 
-function ProductPanelSummary () {
+function ProductPanelSummary (props) {
   return (
-    <Col className='' sm={{ offset: 0, size: 6 }} style={productsColStyle}>
+    <Col className='my-4' sm={{ offset: 0, size: 6 }} style={productsColStyle}>
       <div style={{
         height: '100%',
         backgroundImage: 'url(//images.contentful.com/t9x0u6p47op0/2BxevJTKlY8eieW84SSYOG/687857aff9a9aa18ae1fb92b0bc15bb4/img_product_category_vitamin.jpg)',
@@ -58,12 +68,11 @@ function ProductPanelSummary () {
         justifyContent: 'center',
         alignItems: 'flex-start',
         flexDirection: 'column',
-        padding: 42
+        padding: '42px 64px'
       }}>
-        <h2 className='font-weight-bold mb-4'>Vitamins</h2>
+        <h2 className='font-weight-bold mb-4'>{props.name}</h2>
         <p className=''>
-          Vitamins are compounds of essential nutrients that our bodies need for healthy functioning. They are found in the foods we eat and, in the case of vitamin D, the sun we absorb.<br /><br />
-          Most vitamin supplements on the market are in 'isolate' form, where the capsule contains only the isolated compound. We ferment ours to convert them into a complex form, where they resemble the vitamins you get from real food and are easier to digest.
+          {props.desc}
         </p>
       </div>
     </Col>
@@ -72,39 +81,100 @@ function ProductPanelSummary () {
 
 function ProductPanelItem (props) {
   return (
-    <Col className='' sm={{ offset: 0, size: 3 }} style={productsColStyle}>
-      <div style={{ position: 'relative' }}>
-
-        <a className='' href='/products/vitamin-b-12_1' style={{ color: 'black', textDecoration: 'none' }}>
-          <img className='w-100' src={props.imgSrc} />
-          <div className='font-weight-bold' style={productPanelTitleStyle}>{props.title}</div>
-          <div className='products-type__product-panel-middle'>
-            <div className='products-type__product-panel-icons'>
-              <img src='//assets.contentful.com/t9x0u6p47op0/371OPjK1x6mMuAkykg0mqO/b341ac42bf5f13bfb113917739972d34/ic_topic_energy.svg?w=80&amp;h=80' alt='Ic topic energy' />
-            </div>
-            <p className='products-type__product-panel-description'>
-              • Easy to digest<br />
-              • Fermented for absorption<br />
-              • Formulated for vegans<br />
-              • Essential vitamin for energy production
-            </p>
-            <div className='products-type__product-panel-cta'>Learn more</div>
-          </div>
-          <div className='products-type__product-panel-bottom'>
-            <div className='products-type__product-panel-price'>
-              <h6 className='no-margin'><strong>1 capsule</strong> per day</h6>
-              <h6><strong>$5</strong>/mo</h6>
-            </div>
-            <Button className='btn-danger btn-add-cart btn-lg'>Add to pack</Button>
-          </div>
-        </a>
-
+    <a className='products-type__product-panel col-3 my-4' href='#'>
+      <img className='w-100' src={props.imgSrc} />
+      <div className='font-weight-bold' style={productPanelTitleStyle}>{props.title}</div>
+      <div className='products-type__product-panel-middle'>
+        <div className='products-type__product-panel-icons'>
+          <img src='//assets.contentful.com/t9x0u6p47op0/371OPjK1x6mMuAkykg0mqO/b341ac42bf5f13bfb113917739972d34/ic_topic_energy.svg?w=80&amp;h=80' alt='Ic topic energy' />
+        </div>
+        <p className='products-type__product-panel-description'>
+          {props.shortDesc}
+        </p>
+        <div className='products-type__product-panel-cta'>Learn more</div>
       </div>
-    </Col>
+      <div className='products-type__product-panel-bottom'>
+        <div className='products-type__product-panel-price'>
+          <h6 className='no-margin'><strong>1 capsule</strong> per day</h6>
+          <h6><strong>$5</strong>/mo</h6>
+        </div>
+        <Button className='btn-danger btn-add-cart btn-lg'>Add to pack</Button>
+      </div>
+    </a>
+  )
+}
+
+function CategoryRow (props) {
+  const category = props.category
+  const products = props.products
+
+  // for initial rendering
+  if (!category || !products) return (null)
+
+  // ------ DUMMY DATA START ------
+  const dummyImages = [
+    '//images.contentful.com/t9x0u6p47op0/6AQipVvAtOcEqaiqkekYoU/0a0b21b39692f770fbe77b8f04f95f3e/img_vitaminb12_expert.jpg?w=340&h=190',
+    '//images.contentful.com/t9x0u6p47op0/29RWgFnrvKCGoSUsUMk2OC/43efcf5ec370f7a98f5c76dc536f6571/img_bcomplex3_expert.jpg?w=340&h=190',
+    '//images.contentful.com/t9x0u6p47op0/6Jbetlzyesawuqm6ys2IGG/dbd78634ca497c1469ab4c0aa5b5d171/img_prenatal_expert.jpg?w=340&h=190',
+  ]
+  // ------ DUMMY DATA END   ------
+
+  // after fetch data, this is used
+  return (
+    <Row className='row-eq-height py-5'>
+      <ProductPanelSummary name={category.name} desc={category.desc} />
+      {products.map((product) => (
+        <ProductPanelItem
+          className=''
+          key={product.id + product.name}
+          title={product.name}
+          shortDesc={product.short_desc}
+          imgSrc={dummyImages[_.random(0, 2)]}
+        />
+      ))}
+    </Row>
   )
 }
 
 class Products extends Component {
+  constructor (props) {
+    super(props)
+    this.state = initialState
+  }
+
+  async componentDidMount () {
+    // fetch data from spreadsheets in development-mode
+    try {
+      {
+        const { data } = await axios.get(API.getURL(API.SHEET_PRODUCTS))
+        this.setState({ ...this.state, products: API.csvToObject(data.values) })
+      }
+      {
+        const { data } = await axios.get(API.getURL(API.SHEET_CATEGORIES))
+        this.setState({ ...this.state, categories: API.csvToObject(data.values) })
+      }
+    } catch (e) {
+      console.error(e)
+    }
+  }
+
+  createCategoryRows () {
+    // show spinner, and the div element must be big enough to hide footer
+    if (!this.state.categories.length || !this.state.products.length) {
+      return (<div style={{ height: 2222 }}>
+        <Spinner name='line-scale-pulse-out' fadeIn='none' style={{ position: 'fixed', top: '50%', left: '50%' }} />
+      </div>)
+    }
+
+    return this.state.categories.map((category) => (
+      <CategoryRow
+        key={`CategoryRow-${category.name}`}
+        category={category}
+        products={_.filter(this.state.products, (e) => +e.category_id === +category.id)}
+      />
+    ))
+  }
+
   render () {
     return (
       <div className='animated fadeIn'>
@@ -134,19 +204,8 @@ class Products extends Component {
           </Row>
 
           <section>
-            <Row className='row-eq-height py-5'>
-              <ProductPanelSummary />
-              <ProductPanelItem title='Vitamin B12' imgSrc='//images.contentful.com/t9x0u6p47op0/6AQipVvAtOcEqaiqkekYoU/0a0b21b39692f770fbe77b8f04f95f3e/img_vitaminb12_expert.jpg?w=340&amp;h=190' />
-              <ProductPanelItem title='B-Complex' imgSrc='//images.contentful.com/t9x0u6p47op0/29RWgFnrvKCGoSUsUMk2OC/43efcf5ec370f7a98f5c76dc536f6571/img_bcomplex3_expert.jpg?w=340&h=190' />
-            </Row>
-
-            <hr className='my-0 py-0' />
-
-            <Row className='row-eq-height py-5'>
-              <ProductPanelSummary />
-              <ProductPanelItem title='Vitamin B12' imgSrc='//images.contentful.com/t9x0u6p47op0/6AQipVvAtOcEqaiqkekYoU/0a0b21b39692f770fbe77b8f04f95f3e/img_vitaminb12_expert.jpg?w=340&amp;h=190' />
-              <ProductPanelItem title='B-Complex' imgSrc='//images.contentful.com/t9x0u6p47op0/29RWgFnrvKCGoSUsUMk2OC/43efcf5ec370f7a98f5c76dc536f6571/img_bcomplex3_expert.jpg?w=340&h=190' />
-            </Row>
+            {this.createCategoryRows()}
+            {/* <hr className='my-0 py-0' /> */}
           </section>
         </Container>
       </div>
---
+++ src/utils/api.js
@@ -0,0 +1,19 @@
+// WARN: spreadsheets info (must be used in only dev)
+export const SHEET_PRODUCTS = 'products.csv'
+export const SHEET_CATEGORIES = 'categories.csv'
+export const SHEET_FILTERS = 'filters.csv'
+
+export function getURL (sheetName) {
+  const SPREADSHEET_ID = '1npoKbMd6JJN7iAIS2s-7WCnqNdcyqjhEZhLtBBiJQ8A'
+  const MY_API_KEY = 'AIzaSyBY4rzvROtViiGVQslJnImGqf_2GuWegd0'
+  return `https://sheets.googleapis.com/v4/spreadsheets/${SPREADSHEET_ID}/values/${sheetName}?key=${MY_API_KEY}`
+}
+
+export function csvToObject (data) {
+  return data.slice(1).map((xs) => {
+    return xs.reduce((acc, x, i) => {
+      acc[data[0][i]] = x
+      return acc
+    }, {})
+  })
+}
